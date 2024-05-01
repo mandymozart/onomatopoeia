97d1596171a8f5965d99608a2e55789c6a50dd74
Mandy Mozart <mandymozart@viennastruggle.com>
2024-05-01 16:33:15 +0200

    added header image

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,6 +5,10 @@ import dict from './dict/en.json';
 function App() {
   return (
     <>
+      <img
+        width='100%'
+        src='https://camo.githubusercontent.com/2a58332524817f92453a93fd5701247b364f0f47e58f72bb6ff245c6da8cd02c/687474703a2f2f626c6f672e67656e676f2e636f6d2f77702d636f6e74656e742f75706c6f6164732f323031362f30362f7a61612d7a61612e706e67'
+      />
       <Dictionary dictionary={dict} />
     </>
   );
